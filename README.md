bd5ee54205f346443d69cfd664de2b0292bae0db
TheRedeemed <abdoulkaders77@gmail.com>
2021-03-18 21:34:13 -0400

    Create new activity

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ This portal interacts with [my-activity-expense-tracker-service](https://github.
 - This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 - [Semantic UI](https://github.com/Semantic-Org/Semantic-UI)
-- CSS Grid Layout
-- Flexbox Layout
+- CSS Grid Layout & Flexbox Layout
+- [React Hook Form](https://react-hook-form.com/)
 - [React Testing Library](https://testing-library.com/docs/react-testing-library/intro/)
 - [Jest](https://jestjs.io/)
 - [React-moment](https://www.npmjs.com/package/react-moment)
--- src/components/util/Request.js
+++ src/components/util/Request.js
@@ -1,15 +1,22 @@
 
 const sendRequest = async (requestObj) => {
-    const { requestUrl } = requestObj;
+    const { url, method, request } = requestObj;
     const requestResponse = {
         data: '',
         error: ''
     };
 
     try {
-        let response = await fetch(requestUrl);
+        let response = await fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(request)
+        });
         let data = await response.json();
-        requestResponse.data = data;
+        const { error } = data ;
+        error ? requestResponse.error = error : requestResponse.data = data;
     } catch (error) {
         console.log(error)
         requestResponse.error = error;
